2ab2bb64c4bb68f26d3d9f99a74996730e79b9bb
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2025-07-01 16:42:45 +0700

    server setup

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,32 @@
+// index setup
 const express = require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
 require('dotenv').config();
 
-const sampleRoute = require('');
- app.use(
-    cors({
-        origin : 'http://localhost:5173',
-        credentials : true,
-    })
-)
+// import category route
+const authRoute = require('./routers/authRoute');
+const categoryRoute = require('./routers/categoryRoute');
 
+// enable CORS for frontend 
+app.use(
+  cors({
+    origin: 'http://localhost:5173', // allowed frontend URL
+    credentials: true,               // allow cookies and credentials
+  })
+);
+
+// middleware to parse incoming JSON requests
 app.use(express.json());
-app.use('/v1', sampleRoute);
 
+// use routes
+app.use('/api/v1/auth', authRoute );
+app.use('/api/v1/category', categoryRoute);
+
+// get port from .env
 const PORT = process.env.PORT;
 
+// start the server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
