037084e3ad71a3cf02fedf01fe0305b60d76995f
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2025-07-08 11:27:57 +0700

    add route dashboard hr, finance, employee

--- frontend/src/routers/AppRoute.jsx
+++ frontend/src/routers/AppRoute.jsx
@@ -1,29 +1,42 @@
-import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-// import protected route
-import ProtectedRoute from "./ProtectedRoute.jsx";
-// import login page
-import LoginPage from "../pages/LoginPage.jsx";
-//import layout
-import AppLayout from "../layouts/AppLayout.jsx";
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import ProtectedRoute from '../routers/ProtectedRoute';
+import AppLayout from '../layouts/AppLayout';
+import LoginPage from '../pages/LoginPage';
+
+import HRDashboard from '../pages/hr/Dashboard';
+import FinanceDashboard from '../pages/finance/Dashboard';
+import EmployeeDashboard from '../pages/employee/Dashboard';
 
 const AppRoute = () => (
   <Routes>
-    {/* public route */}
     <Route path="/" element={<LoginPage />} />
 
-    {/* protected route */}
-    <Route element={<ProtectedRoute/>}>
+    {/* Human Resources Route */}
+    <Route element={<ProtectedRoute allowedRoles={['HR']} />}>
       <Route element={<AppLayout />}>
-      {/* dashboard */}
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/hr/dashboard" element={<HRDashboard />} />
       </Route>
     </Route>
 
-    {/* catch all */}
-    <Route path="*" element={<Navigate to="/" />} />
+    {/* Finance Route */}
+    <Route element={<ProtectedRoute allowedRoles={['Finance']} />}>
+      <Route element={<AppLayout />}>
+        <Route path="/finance/dashboard" element={<FinanceDashboard />} />
+      </Route>
+    </Route>
+
+    {/* Employee Route */}
+    <Route element={<ProtectedRoute allowedRoles={['Employee']} />}>
+      <Route element={<AppLayout />}>
+        <Route path="/employee/dashboard" element={<EmployeeDashboard />} />
+      </Route>
+    </Route>
+
+    {/* Catch All */}
+    <Route path="*" element={<Navigate to="/" replace />} />
   </Routes>
 );
-
 
 export default AppRoute;
