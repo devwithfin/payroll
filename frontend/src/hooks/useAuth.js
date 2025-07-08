36eb759eb723fe11732f7fe0cfbacba97ac22ece
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2025-07-08 11:24:51 +0700

    auth hooks

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,4 +1,4 @@
-// hooks/auth 
+// hooks/useAuth.js
 import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
@@ -15,52 +15,58 @@ export default function useAuth() {
     try {
       const { data } = await login(username, password);
 
+      const role = data.user.role; 
+      
       localStorage.setItem("token", data.token);
-      localStorage.setItem("id_user", data.id);
-      localStorage.setItem("username", data.username);
-      localStorage.setItem("role_name", data.role_name);
+      localStorage.setItem("username", data.user.username);
+      localStorage.setItem("role", role);
+
+      let redirectPath = "/";
+      if (role === "HR") redirectPath = "/hr/dashboard";
+      else if (role === "Finance") redirectPath = "/finance/dashboard";
+      else if (role === "Employee") redirectPath = "/employee/dashboard";
 
       Swal.fire({
         icon: "success",
         title: "Login Successfully!",
-        text: "Welcome Back!",
+        text: "Welcome back!",
         timer: 1000,
         showConfirmButton: false,
+        timerProgressBar: true,
+        willClose: () => {
+          navigate(redirectPath);
+        }
       });
 
-      setTimeout(() => {
-        window.location.href = "/admin/dashboard";
-      }, 1000);
     } catch (error) {
-      const message = error.response?.data?.message || "Please Try Again!";
+      const message = error.response?.data?.message || "Please try again!";
       Swal.fire({
         icon: "error",
         title: "Failed to Login",
         text: message,
       });
     }
-  }, []);
+  }, [navigate]);
 
   const handleLogout = useCallback(async () => {
     try {
       const token = localStorage.getItem("token");
       await logout(token);
-
+      localStorage.clear();
       Swal.fire({
         icon: "success",
-        title: "Successful Logout",
-        text: "You're out. See you again!",
-        showConfirmButton: false,
+        title: "Logout Successful",
+        text: "See you again!",
         timer: 1000,
+        showConfirmButton: false,
+      }).then(() => {
+        navigate("/");
       });
-      localStorage.clear();
-      navigate("/");
     } catch (error) {
-      const message = error.response?.data?.message || "Please Try Again!";
       Swal.fire({
         icon: "error",
-        title: "Oops..",
-        text: message,
+        title: "Logout Failed",
+        text: error.response?.data?.message || "Please try again!",
       });
     }
   }, [navigate]);
