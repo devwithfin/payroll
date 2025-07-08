d74d2db7a93a87a69302cadd187ef7b4eb2e28a5
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2025-07-08 11:30:01 +0700

    auth service

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -3,10 +3,10 @@ import API from '../libs/axiosInstance';
 export const get = () => API.get('/');
 
 export const login = (username, password) =>
-  API.post("/login", { username, password });
+  API.post("/auth/login", { username, password });
 
 export const logout = (token) =>
-  API.post("/logout", {}, {
+  API.post("/auth/logout", {}, {
     headers: { Authorization: `Bearer ${token}` },
   });
 
