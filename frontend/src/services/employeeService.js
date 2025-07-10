c0d832250cf9988dfc2fdcd36a35e566479a1c4a
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2025-07-10 16:03:55 +0700

    service api employee

--- frontend/src/services/employeeService.js
+++ frontend/src/services/employeeService.js
@@ -1,1 +1,16 @@
 // service/employee
+// services/department
+import API from '../libs/axiosInstance';
+
+export const getAllEmployees = () => API.get('/employees');
+
+export const getEmployeeById = (id) => API.get(`/employees/${id}`);
+
+export const createEmployee = (data) =>
+  API.post('/employees', data);
+
+export const updateEmployee = (id, data) =>
+  API.put(`/employees/${id}`, data);
+
+export const deleteEmployee = (id) =>
+  API.delete(`/employees/${id}`);
