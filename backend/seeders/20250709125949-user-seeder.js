af62d75f3f17e4e86502c712705037942afa077a
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2025-07-10 15:56:49 +0700

    update data table

---
+++ backend/seeders/20250709125949-user-seeder.js
@@ -0,0 +1,36 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("users", [
+      {
+        username: "bayuanggara@gmail.com",  
+        password: "$2b$10$bKgw9Q4RG.HaPk5Aj.XlPuFBbPT8/prBGDX6W7siH80SSO06xz4yW",
+        role: "HR",
+        employee_id: 1,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        username: "dewipustis@gmail.com",
+        password: "$2b$10$bKgw9Q4RG.HaPk5Aj.XlPuFBbPT8/prBGDX6W7siH80SSO06xz4yW",
+        role: "Finance",
+        employee_id: 2,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        username: "alfiansyahcahyow@gmail.com",
+        password: "$2b$10$bKgw9Q4RG.HaPk5Aj.XlPuFBbPT8/prBGDX6W7siH80SSO06xz4yW",
+        role: "Employee",
+        employee_id: 3,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("users", null, {});
+  },
+};
