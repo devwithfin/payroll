f86ef180c69b576c01a8cfb0c21c82d597bdc4a3
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2025-07-10 16:01:30 +0700

    update employee route

--- backend/routers/employeeRoute.js
+++ backend/routers/employeeRoute.js
@@ -1,1 +1,16 @@
-// router/department
+// router/employee
+const express = require("express");
+const router = express.Router();
+const employeeController = require("../controllers/employeeController");
+
+router.get("/", employeeController.getAll);
+
+router.get("/:id", employeeController.getById);
+
+router.post("/", employeeController.create);
+
+router.put("/:id", employeeController.update);
+
+router.delete("/:id", employeeController.destroy);
+
+module.exports = router;
