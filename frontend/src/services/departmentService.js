3bde73e7c117d3ecbfb43745ec77cca43c725f3a
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2025-07-10 16:03:32 +0700

    service api department

--- frontend/src/services/departmentService.js
+++ frontend/src/services/departmentService.js
@@ -1,1 +1,16 @@
 // services/department
+// services/position
+import API from '../libs/axiosInstance';
+
+export const getAllDepartments = () => API.get('/departments');
+
+export const getDepartmentById = (id) => API.get(`/departments/${id}`);
+
+export const createDepartment = (data) =>
+  API.post('/departments', data);
+
+export const updateDepartment = (id, data) =>
+  API.put(`/departments/${id}`, data);
+
+export const deleteDepartment = (id) =>
+  API.delete(`/departments/${id}`);
