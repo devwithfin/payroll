fd5769f955b8cbbb970c1bf2b1ccd90326638418
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2025-07-11 14:37:28 +0700

    update seeder

--- backend/seeders/20250707055457-position-seeder.js
+++ backend/seeders/20250707055457-position-seeder.js
@@ -3,20 +3,20 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('positions', [
-      { position_name: 'Software Engineer', base_salary: 10000000, created_at: new Date(), updated_at: new Date() },
-      { position_name: 'Frontend Developer', base_salary: 9000000, created_at: new Date(), updated_at: new Date() },
-      { position_name: 'Backend Developer', base_salary: 9000000, created_at: new Date(), updated_at: new Date() },
-      { position_name: 'DevOps Engineer', base_salary: 11000000, created_at: new Date(), updated_at: new Date() },
-      { position_name: 'QA Engineer', base_salary: 8500000, created_at: new Date(), updated_at: new Date() },
-      { position_name: 'Product Manager', base_salary: 13000000, created_at: new Date(), updated_at: new Date() },
-      { position_name: 'UI/UX Designer', base_salary: 8000000, created_at: new Date(), updated_at: new Date() },
-      { position_name: 'HR Generalist', base_salary: 7000000, created_at: new Date(), updated_at: new Date() },
-      { position_name: 'Finance Analyst', base_salary: 8000000, created_at: new Date(), updated_at: new Date() },
-      { position_name: 'IT Support', base_salary: 6000000, created_at: new Date(), updated_at: new Date() },
-      { position_name: 'Customer Support Specialist', base_salary: 5500000, created_at: new Date(), updated_at: new Date() },
-      { position_name: 'Marketing Specialist', base_salary: 6500000, created_at: new Date(), updated_at: new Date() },
-      { position_name: 'Sales Executive', base_salary: 7500000, created_at: new Date(), updated_at: new Date() },
-      { position_name: 'Office Manager', base_salary: 6500000, created_at: new Date(), updated_at: new Date() }
+      { position_name: 'Software Engineer', base_salary: 10000000, job_allowance: 500000, created_at: new Date(), updated_at: new Date() },
+      { position_name: 'Frontend Developer', base_salary: 9000000, job_allowance: 400000, created_at: new Date(), updated_at: new Date() },
+      { position_name: 'Backend Developer', base_salary: 9000000, job_allowance: 400000, created_at: new Date(), updated_at: new Date() },
+      { position_name: 'DevOps Engineer', base_salary: 11000000, job_allowance: 600000, created_at: new Date(), updated_at: new Date() },
+      { position_name: 'QA Engineer', base_salary: 8500000, job_allowance: 400000, created_at: new Date(), updated_at: new Date() },
+      { position_name: 'Product Manager', base_salary: 13000000, job_allowance: 1000000, created_at: new Date(), updated_at: new Date() },
+      { position_name: 'UI/UX Designer', base_salary: 8000000, job_allowance: 350000, created_at: new Date(), updated_at: new Date() },
+      { position_name: 'HR Generalist', base_salary: 7000000, job_allowance: 300000, created_at: new Date(), updated_at: new Date() },
+      { position_name: 'Finance Analyst', base_salary: 8000000, job_allowance: 300000, created_at: new Date(), updated_at: new Date() },
+      { position_name: 'IT Support', base_salary: 6000000, job_allowance: 250000, created_at: new Date(), updated_at: new Date() },
+      { position_name: 'Customer Support Specialist', base_salary: 5500000, job_allowance: 200000, created_at: new Date(), updated_at: new Date() },
+      { position_name: 'Marketing Specialist', base_salary: 6500000, job_allowance: 300000, created_at: new Date(), updated_at: new Date() },
+      { position_name: 'Sales Executive', base_salary: 7500000, job_allowance: 400000, created_at: new Date(), updated_at: new Date() },
+      { position_name: 'Office Manager', base_salary: 6500000, job_allowance: 350000, created_at: new Date(), updated_at: new Date() },
     ], {});
   },
 
--- backend/seeders/20250707061520-allowance-seeder.js
+++ backend/seeders/20250707061520-allowance-seeder.js
@@ -1,44 +1,43 @@
-'use strict';
+"use strict";
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('allowances', [
-      {
-        allowance_name: 'Tunjangan Transport',
-        is_taxable: true,
-        is_fixed: true
-      },
-      {
-        allowance_name: 'Tunjangan Makan',
-        is_taxable: true,
-        is_fixed: true
-      },
-      {
-        allowance_name: 'Tunjangan Komunikasi',
-        is_taxable: true,
-        is_fixed: true
-      },
-      {
-        allowance_name: 'Tunjangan Jabatan',
-        is_taxable: true,
-        is_fixed: true
-      },
-      {
-        allowance_name: 'Tunjangan Keahlian',
-        is_taxable: true,
-        is_fixed: false
-      },
-      {
-        allowance_name: 'Tunjangan Keluarga',
-        is_taxable: true,
-        is_fixed: true
-      }
-    ], {});
+    await queryInterface.bulkInsert(
+      "allowances",
+      [
+        {
+          allowance_name: "Tunjangan Makan",
+          is_fixed: true,
+          is_taxable: true,
+          default_amount: 1000000,
+        },
+        {
+          allowance_name: "Tunjangan Transport",
+          is_fixed: true,
+          is_taxable: true,
+          default_amount: 1000000,
+        },
+        {
+          allowance_name: "Tunjangan Jabatan",
+          is_fixed: true,
+          is_taxable: true,
+          default_amount: null,
+        },
+        {
+          allowance_name: "Tunjangan Kinerja",
+          is_fixed: false,
+          is_taxable: true,
+          default_amount: null,
+        },
+      ],
+      {}
+    );
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('allowances', null, {});
-    await queryInterface.sequelize.query('ALTER TABLE allowances AUTO_INCREMENT = 1');
-
-  }
+    await queryInterface.bulkDelete("allowances", null, {});
+    await queryInterface.sequelize.query(
+      "ALTER TABLE allowances AUTO_INCREMENT = 1"
+    );
+  },
 };
