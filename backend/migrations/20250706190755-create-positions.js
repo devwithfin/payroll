611bf91a818c25ee235bce35c2a2a4de720c2b81
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2025-07-11 14:37:02 +0700

    update migration

--- backend/migrations/20250706190755-create-positions.js
+++ backend/migrations/20250706190755-create-positions.js
@@ -1,43 +1,47 @@
-'use strict';
+"use strict";
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-  await queryInterface.createTable('positions', {
-  position_id: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-    autoIncrement: true,
-    primaryKey: true
-  },
-  position_name: {
-    type: Sequelize.STRING(100),
-    allowNull: false,
-    unique: true
-  },
-  base_salary: {
-    type: Sequelize.DECIMAL(15, 2),
-    allowNull: false
-  },
-  created_at: {
-    type: Sequelize.DATE,
-    allowNull: false,
-    defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-  },
-  updated_at: {
-    type: Sequelize.DATE,
-    allowNull: false,
-    defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-  },
-  deleted_at: {
-    type: Sequelize.DATE,
-    allowNull: true,
-    defaultValue: null
-  }
-});
-
+    await queryInterface.createTable("positions", {
+      position_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      position_name: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        unique: true,
+      },
+      base_salary: {
+        type: Sequelize.DECIMAL(15, 2),
+        allowNull: false,
+      },
+      job_allowance: {
+        type: Sequelize.DECIMAL(15, 2),
+        allowNull: false,
+        defaultValue: 0,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: null,
+      },
+    });
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('positions');
-  }
+    await queryInterface.dropTable("positions");
+  },
 };
--- backend/migrations/20250706192102-create-allowances.js
+++ backend/migrations/20250706192102-create-allowances.js
@@ -1,31 +1,35 @@
-'use strict';
+"use strict";
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('allowances', {
+    await queryInterface.createTable("allowances", {
       allowance_id: {
         type: Sequelize.INTEGER,
         allowNull: false,
         autoIncrement: true,
-        primaryKey: true
+        primaryKey: true,
       },
       allowance_name: {
         type: Sequelize.STRING(100),
         allowNull: false,
-        unique: true
+        unique: true,
       },
       is_taxable: {
         type: Sequelize.BOOLEAN,
-        defaultValue: false
+        defaultValue: false,
       },
       is_fixed: {
         type: Sequelize.BOOLEAN,
-        defaultValue: false
+        defaultValue: false,
+      },
+      default_amount: {
+        type: Sequelize.DECIMAL(15, 2),
+        allowNull: true,
       },
     });
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('allowances');
-  }
+    await queryInterface.dropTable("allowances");
+  },
 };
