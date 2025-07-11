c75e22576ca533cde05dbd4614a869735a5c1d9a
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2025-07-11 11:15:52 +0700

    db models attendance

---
+++ backend/models/attendance.js
@@ -0,0 +1,57 @@
+// model/attedance
+"use strict";
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class Attendance extends Model {
+    static associate(models) {
+      Attendance.belongsTo(models.Employee, {
+        foreignKey: "employee_id",
+        as: "employee",
+      });
+    }
+  }
+
+  Attendance.init(
+    {
+      attendance_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      attendance_date: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+      },
+      check_in_time: {
+        type: DataTypes.TIME,
+        allowNull: false,
+      },
+      check_out_time: {
+        type: DataTypes.TIME,
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.ENUM("Present", "Sick", "Leave", "Absent"),
+        allowNull: false,
+      },
+      notes: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      status_processed: {
+        type: DataTypes.ENUM("Unprocessed", "Processed"),
+        allowNull: false,
+        defaultValue: "Unprocessed",
+      },
+    },
+    {
+      sequelize,
+      modelName: "Attendance",
+      tableName: "attendances",
+      timestamps: true,
+    }
+  );
+
+  return Attendance;
+};
