d42ca6124ab46f2660a3104981dc79f0b29934bd
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2025-07-11 18:45:01 +0700

    update add new field department

--- backend/models/position.js
+++ backend/models/position.js
@@ -1,42 +1,64 @@
-// model/position
-'use strict';
-const { Model } = require('sequelize');
+"use strict";
 
 module.exports = (sequelize, DataTypes) => {
-  class Position extends Model {
-    static associate(models) {
-      Position.hasMany(models.Employee, {
-        foreignKey: 'position_id'
-      });
+  const Position = sequelize.define(
+    "Position",
+    {
+      position_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      department_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      position_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      base_salary: {
+        type: DataTypes.DECIMAL(15, 2),
+        allowNull: false,
+      },
+      job_allowance: {
+        type: DataTypes.DECIMAL(15, 2),
+        allowNull: true,
+        defaultValue: 0.00,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      deleted_at: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+    },
+    {
+      tableName: "positions",
+      underscored: true,
+      paranoid: true, // untuk soft delete
+      timestamps: true, // aktifkan otomatis createdAt & updatedAt
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+      deletedAt: "deleted_at",
     }
-  }
+  );
 
-  Position.init({
-    position_id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  position_name: {
-    type: DataTypes.STRING(100),
-    allowNull: false,
-    unique: true
-  },
-  base_salary: {
-    type: DataTypes.DECIMAL(15, 2),
-    allowNull: false
-  }
-}, {
-  sequelize,
-  modelName: 'Position',
-  tableName: 'positions',
-  timestamps: true,
-  createdAt: 'created_at',
-  updatedAt: 'updated_at',
-  paranoid: true,
-  deletedAt: 'deleted_at'
-});
-
+  // Relasi ke Department
+  Position.associate = function (models) {
+    Position.belongsTo(models.Department, {
+      foreignKey: "department_id",
+      as: "department",
+    });
+  };
 
   return Position;
 };
