366c9c5a512a45b5d43a6550403695ec038f617e
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2025-07-12 14:45:21 +0700

    service api attendance

---
+++ frontend/src/services/attendanceService.js
@@ -0,0 +1,15 @@
+// services/position
+import API from '../libs/axiosInstance';
+
+export const getAllAttendances = () => API.get('/attendances');
+
+export const geAllAttendanceById = (id) => API.get(`/attendances/${id}`);
+
+// export const createAllAttendance = (data) =>
+//   API.post('/attendances', data);
+
+// export const updateAllAttendance = (id, data) =>
+//   API.put(`/attendances/${id}`, data);
+
+// export const deleteAllAttendance = (id) =>
+//   API.delete(`/attendances/${id}`);
