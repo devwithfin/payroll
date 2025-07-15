844f2e90fbd4ed9e0186a811b780783a194560ad
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2025-07-16 00:56:51 +0700

    separate contexts and auth structure

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,15 @@
-// main
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import { AuthProvider } from './contexts/AuthContext.jsx'; //
+import AuthProvider from './providers/AuthProvider.jsx';  // ✅ yang benar
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-     <AuthProvider>  
+      <AuthProvider>
         <App />
       </AuthProvider>
     </BrowserRouter>
