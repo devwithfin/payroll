7bb98864db8f34df7e48f19ed3a3e526b3ca9660
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2025-07-16 00:59:01 +0700

    change field username to email for login

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -4,11 +4,12 @@ import API from '../libs/axiosInstance';
 
 export const get = () => API.get('/');
 
-export const login = (username, password) =>
-  API.post("/auth/login", { username, password });
+export const login = (email, password) =>
+  API.post("/auth/login", { email, password });
 
 export const getProfile = () =>
   API.get("/auth/profile");
+
 export const logout = () =>
   API.post("/auth/logout");
 
