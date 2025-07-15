434255377ba11f070406ff2191601706c1f77b1a
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2025-07-16 01:03:05 +0700

    change path dir

--- frontend/src/pages/employee/Profile.jsx
+++
@@ -1,4 +0,0 @@
-// pages/employee/profile
-export default function Profile() {
-  return <h2></h2>;
-}
--- frontend/src/pages/hr/Profile.jsx
+++
@@ -1,4 +0,0 @@
-// pages/hr/profile
-export default function Profile() {
-  return <h2></h2>;
-}
