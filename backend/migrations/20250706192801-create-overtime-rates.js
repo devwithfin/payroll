806875add134d8e7611407e761d081e3b93b3dad
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2025-07-20 23:53:48 +0700

    update migrations

--- backend/migrations/20250706192801-create-overtime-rates.js
+++ backend/migrations/20250706192801-create-overtime-rates.js
@@ -20,6 +20,21 @@ module.exports = {
       },
       description: {
         type: Sequelize.TEXT
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      deleted_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: null
       }
     });
   },
