70003b99da97d9ef966a49ea2cfdc3cb34ce9471
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2025-07-20 19:08:48 +0700

    add api service deduction

---
+++ frontend/src/services/deductionService.js
@@ -0,0 +1,17 @@
+
+// service/deduction-service
+import API from '../libs/axiosInstance';
+
+export const getAllDeductions = () => API.get('/deductions');
+
+export const getDeductionById = (id) =>
+  API.get(`/deductions/${id}`);
+
+export const createDeduction = (data) =>
+  API.post('/deductions', data);
+
+export const updateDeduction = (id, data) =>
+  API.put(`/deductions/${id}`, data);
+
+export const deleteDeduction = (id) =>
+  API.delete(`/deductions/${id}`);
